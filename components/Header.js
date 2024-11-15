7fdc5b6596fad79aaab84835d29ab1c4cbdced42
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-15 22:52:12 +0200

    Created and used Header component

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+      <header className="header">
+        <img 
+          src="./assets/troll-face.png" 
+          className="header--image"
+        />
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project">React Course - Project 3</h4>
+      </header>
+    );
+  }
+  export default Header;
