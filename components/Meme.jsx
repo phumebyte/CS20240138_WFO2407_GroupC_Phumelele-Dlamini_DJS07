bf4e3059b1d318712f2eb5fda0525f2ab4883b99
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-15 23:12:56 +0200

    Added meme image generator file

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,23 +1,34 @@
+import React from "react"
+import memesData from "../memesData.js"
+
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("") // Initial state for memeImage is fine
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        if (memesArray.length > 0) { // Check if memesArray is not empty
+            const randomNumber = Math.floor(Math.random() * memesArray.length)
+            setMemeImage(memesArray[randomNumber].url)
+        } else {
+            console.error("No memes available") // Log an error if no memes are available
+        }
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
-                    className="form--input"
-                />
-                <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form--input"
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
+            {memeImage && <img src={memeImage} className="meme--image" alt="Meme" />} {/* Add alt text for accessibility */}
         </main>
     )
 }
